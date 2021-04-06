b3cbc2b621813a4fa76825ed7ac473bd3086850d
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-06 15:38:39 -0400

    costructor created

--- geolocation/src/app/home/home.page.ts
+++ geolocation/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocationPlaces } from 'src/location/places.location';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(public router: Router) {
+  constructor(public router: Router,public locationPlaces: LocationPlaces) {
 
   }
 
