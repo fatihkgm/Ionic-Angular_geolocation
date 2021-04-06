e6c1577840c6e36fd58a3bb5003c7c418374bc35
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-06 13:54:57 -0400

    navigation page for place implmeneted

--- geolocation/src/app/place/place.page.ts
+++ geolocation/src/app/place/place.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-place',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place.page.scss'],
 })
 export class PlacePage implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  goBack() {
+    this.router.navigate(['home']);
   }
-
 }
