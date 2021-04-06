a8a093df3ea07b07e869c8daab16667fa73b6706
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-06 12:26:14 -0400

    new place page created

--- geolocation/src/app/app-routing.module.ts
+++ geolocation/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'place',
+    loadChildren: () => import('./place/place.module').then( m => m.PlacePageModule)
+  },
 ];
 
 @NgModule({
